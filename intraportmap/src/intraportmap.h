c84a9e47e7fa5fd5c9e80c9b750a37f8a41c6859
qxyzm <qxyzm@localhost>
2022-07-17 20:51:41 +0800

    服务结构体设计方案

--- intraportmap/src/intraportmap.h
+++ intraportmap/src/intraportmap.h
@@ -1,6 +1,74 @@
 #ifndef _INTRAPORTMAP_H
 #define _INTRAPORTMAP_H
 
+// 配对完成后的转发tunnel，服务器端收到立即自增建立
+class ipm_tunnel_server
+{
+public:
+	// 这一头
+	// 那一头
+
+	// 状态 （c未通知 c就绪 b未连接 b就绪）
+
+	// 这一头(读写挂)（用两个函数区分两头）
+	// 那一头(读写挂)
+};
+
+// 服务器端的用户fd服务模块，多个，listen来访客立即建立
+class ipm_agent_server
+{
+public:
+	// 客户端主连接
+	// 服务器代理listen
+
+	// listen结果
+	// 客户端结果(读写挂)
+
+	// 不存tunnel tunnel查找需要上层
+};
+
+// 服务器端（一个）
+class ipm_server
+{
+public:
+	// 服务器listen
+
+	// listen结果（读写挂）
+
+	// 主动跑ipm_agent_server，或tunnel
+
+	// 存一堆ipm_agent_server
+	// 存一堆ipm_tunnel，方便查找
+};
+
+// 配对完成后的转发tunnel，客户端收到控制信号编号立即建立
+class ipm_tunnel_client
+{
+public:
+	// 这一头
+	// 那一头
+
+	// 状态 （a未连接 a就绪 b未连接 b未协商 b就绪）
+
+	// 这一头(读写挂)（用两个函数区分两头）
+	// 那一头(读写挂)
+};
+
+// 客户端（一个）
+class ipm_client
+{
+public:
+	// 服务器的IP
+	// 连接
+
+	// DNS结果（有挂）
+	// 服务器结果（读写挂）（挂了重连不退确保tunnel好的）
+
+	// 主动跑ipm_tunnel，内部即可
+
+	// 存一堆tunnel
+};
+
 class intraportmap
 {
 public:
